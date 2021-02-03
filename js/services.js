74973d1fddf1f65e88d19cda482899ed513cac23
autoex <autoex@gmail.com>
2021-02-03 11:20:21 +0200

    replace jquery on axios

--- js/services.js
+++ js/services.js
@@ -1,7 +1,13 @@
-function getImages(page = 1, count = 3, callBack) {
+/* function getImages(page = 1, count = 3) {
 
   return   $.ajax(`https://repetitora.net/api/JS/Images?page=${page}&count=${count}`).then(data=>  data);
 
   
   
+} */
+
+function getImages(page = 1, count = 3) {
+  return axios
+    .get(`https://repetitora.net/api/JS/Images?page=${page}&count=${count}`)
+    .then((data) => data.data);
 }
